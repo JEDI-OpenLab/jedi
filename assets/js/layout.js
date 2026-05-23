5176b9ad2591c38289cdc4e3042f23a5a6eac03e
JEDI-OpenLab <jean.debaecker@centralelille.fr>
2026-05-23 11:06:08 +0200

    Add files via upload

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,5 +1,5 @@
 (() => {
-  const siteVersion = "2026-05-23.1";
+  const siteVersion = "2026-05-23.2";
   const siteTagline = "Pédagogie ouverte, conçue avec soin.";
   const getTheme = () => document.documentElement.getAttribute("data-bs-theme") || "light";
   const pathname = window.location.pathname;
